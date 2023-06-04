68fbc57b285141043b06cb7cf9008cc17ac33f4c
Burak Ustun <burakustun54@hotmail.com>
2023-06-04 15:41:46 +0300

    v3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
   console.log(loading);
   return (
     <>
+    <p>asd</p>
       <form className="max-w-screen-sm mx-auto mt-4">
         <div className="relative">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
